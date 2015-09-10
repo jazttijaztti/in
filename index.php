ca0c8e52a3af92bac96eb2f53c6162ceecab5242
ferdinand <you@example.com>
2015-09-10 14:42:20 +0700

    add new data

--- index.php
+++ index.php
@@ -7,4 +7,10 @@ echo "a";
 >>>>>>> a19053f475a74377f6d998a77419eac5f9a60587
 phpinfo();
 echo "bbb";
+
+echo "data from Rr";
+
+
+echo "data from qqq";
+
 ?>
---
+++ index_master2.php
@@ -0,0 +1,4 @@
+<?php
+echo "this is index_master2.php";
+
+?>
