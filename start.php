23fe1f39a246a06da3842581ae6907a288bfd4ab
eric <you@example.com>
2015-09-11 09:24:52 +0000

    start.php revision 001

--- start.php
+++ start.php
@@ -2,34 +2,31 @@
 <head>
 <meta charset="utf-8">
 </head>
-
-    <body>
-    
-		<h1>REGISTRATION FORM</h1>
-				
-		<form action="send.php" method="POST">
-        
-			E-Mail <input type="email" name="email"><br><br>
+<body>
+<h1>REGISTRATION FORM</h1>
+<form action="send.php" method="POST">
+     
+E-Mail <input type="email" name="email"><br><br>
             
-			Name <input type="text" name="name"><br><br>
+Name <input type="text" name="name"><br><br>
 			
-			Date of Birth <input type="date" name="dateofbirth"><br><br>
+Date of Birth <input type="date" name="dateofbirth"><br><br>
             
-            Gender	<input type="radio" name="gender" value="male" checked>Male 	<input type="radio" name="gender" value="female">Female<br><br>
+Gender	<input type="radio" name="gender" value="male" checked>Male 	<input type="radio" name="gender" value="female">Female<br><br>
             
-            Address <input type="text" name="address"><br><br>
+Address <input type="text" name="address"><br><br>
 			
-            Phone Number <input type="text" name="phone"><br><br>
+Phone Number <input type="text" name="phone"><br><br>
             
-            Certification <br>
-			<input type="checkbox" name="certificate[]"> Nurse <br>
-			<input type="checkbox" name="certificate[]"> Childrearing <br>
-			<input type="checkbox" name="certificate[]"> Consultation <br><br>
+Certification <br>
+<input type="checkbox" name="certificate[]" value="nurse"> Nurse <br>
+<input type="checkbox" name="certificate[]" value="childrearing"> Childrearing <br>
+<input type="checkbox" name="certificate[]" value="consultation"> Consultation <br><br>
             
-            <button type="submit" class="btn btn-large btn-primary" name="submit">Register</button>
+<button type="submit" class="btn btn-large btn-primary" name="submit">Register</button>
 			
-		</form>
+</form>
 	
-	</body>
+</body>
     
 </html>
